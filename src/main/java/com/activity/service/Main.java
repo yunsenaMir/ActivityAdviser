71e009a9946cdd6ce6cb191789fc601be648f68f
Aysena Mironova <mironova.av@granit-concern.ru>
2021-11-06 14:10:34 +0300

    some modifications of the interface added

--- src/main/java/com/activity/service/Main.java
+++ src/main/java/com/activity/service/Main.java
@@ -11,8 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
